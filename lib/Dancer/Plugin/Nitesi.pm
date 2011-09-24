fc3fa573645ba0368307878011ea41dc0e5cb428
Stefan Hornburg (Racke) <racke@linuxia.de>
2011-09-24 19:55:45 +0200

    Bump up version number.

--- lib/Dancer/Plugin/Nitesi.pm
+++ lib/Dancer/Plugin/Nitesi.pm
@@ -18,11 +18,11 @@ Dancer::Plugin::Nitesi - Nitesi Shop Machine plugin for Dancer
 
 =head1 VERSION
 
-Version 0.0002
+Version 0.0003
 
 =cut
 
-our $VERSION = '0.0002';
+our $VERSION = '0.0003';
 
 =head1 SYNOPSIS
 
